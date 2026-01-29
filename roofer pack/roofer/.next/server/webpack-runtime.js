d2b3a6a7f62a8946d0a6ee40f530b992c2dd4880
Klisman <klisoved28@gmail.com>
2026-01-29 08:39:38 -0600

    change AI 2

--- roofer pack/roofer/.next/server/webpack-runtime.js
+++ roofer pack/roofer/.next/server/webpack-runtime.js
@@ -47,6 +47,11 @@
 /******/ 	__webpack_require__.m = __webpack_modules__;
 /******/ 	
 /************************************************************************/
+/******/ 	/* webpack/runtime/amd options */
+/******/ 	(() => {
+/******/ 		__webpack_require__.amdO = {};
+/******/ 	})();
+/******/ 	
 /******/ 	/* webpack/runtime/compat get default export */
 /******/ 	(() => {
 /******/ 		// getDefaultExport function for compatibility with non-harmony modules
@@ -125,7 +130,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("8eb51f84d442db59")
+/******/ 		__webpack_require__.h = () => ("09223a19a7f47d85")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
