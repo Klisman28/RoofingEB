3011c8f85753bb91749f45a5353f1f61299a324c
Klisman Oved Aguirre Méndez <klisman@MacBook-Air-de-Klisman.local>
2026-08-20 12:59:36 -0600

    Add Meta Pixel tracking sitewide

--- roofer pack/roofer/app/layout.js
+++ roofer pack/roofer/app/layout.js
@@ -5,6 +5,7 @@ import 'swiper/css'
 import "swiper/css/pagination"
 // import 'owl.carousel'
 import 'swiper/css/free-mode';
+import Script from 'next/script'
 import { inter, roboto } from '@/lib/font'
 export const metadata = {
     title: 'E&B Roofing',
@@ -17,7 +18,30 @@ export const metadata = {
 export default function RootLayout({ children }) {
     return (
         <html lang="en" className={`${inter.variable} ${roboto.variable}`}>
-            <body>{children}</body>
+            <body>
+                {children}
+                <Script id="meta-pixel" strategy="afterInteractive">
+                    {`!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '2036724506972192');
+fbq('track', 'PageView');`}
+                </Script>
+                <noscript>
+                    <img
+                        height="1"
+                        width="1"
+                        style={{ display: 'none' }}
+                        src="https://www.facebook.com/tr?id=2036724506972192&ev=PageView&noscript=1"
+                        alt=""
+                    />
+                </noscript>
+            </body>
         </html>
     )
 }
