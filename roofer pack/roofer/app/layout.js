67a5e77bdacc88966760ce71d9d5e80c60a6c8e6
Klisman Oved Aguirre Méndez <klisman@MacBook-Air-de-Klisman.local>
2026-08-21 14:00:00 -0600

    Update Meta Pixel identifier

--- roofer pack/roofer/app/layout.js
+++ roofer pack/roofer/app/layout.js
@@ -89,7 +89,7 @@ n.queue=[];t=b.createElement(e);t.async=!0;
 t.src=v;s=b.getElementsByTagName(e)[0];
 s.parentNode.insertBefore(t,s)}(window, document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '2036724506972192');
+fbq('init', '1054929873825935');
 fbq('track', 'PageView');`}
                 </Script>
                 <noscript>
@@ -97,7 +97,7 @@ fbq('track', 'PageView');`}
                         height="1"
                         width="1"
                         style={{ display: 'none' }}
-                        src="https://www.facebook.com/tr?id=2036724506972192&ev=PageView&noscript=1"
+                        src="https://www.facebook.com/tr?id=1054929873825935&ev=PageView&noscript=1"
                         alt=""
                     />
                 </noscript>
